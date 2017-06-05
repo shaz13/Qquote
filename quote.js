a93875aeb39abbf7feead8fcb8cc480de755e838
Shahebaz <shahebaz13@gmail.com>
2017-06-06 01:10:37 +0530

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("#get").click(function () {
+
+        jQuery(function ($) {
+            console.log("start")
+            $.get("quran_quotes.txt", function (wholeTextFile) {
+                console.log(wholeTextFile)
+                var lines = wholeTextFile.split(/\n/),
+                    randomIndex = Math.floor(Math.random() * lines.length),
+                    randomLine = lines[randomIndex];
+                console.log(randomIndex, randomLine);
+                $.get("quran_ref_quotes.txt", function (wholeTextFile) {
+                    var lines = wholeTextFile.split(/\n/),
+                    random_ref = lines[randomIndex];
+                    console.log(random_ref, randomLine);
+                    var url = "https://quran.com/" + random_ref
+                    var target = "_blank"
+                    $("#quran_ref_quotes").hide();
+                    $("#quran_ref_quotes").html(" <a href ="+ url + " target=" + target + " > Quran " + random_ref.replace(/#/g, "<br>" + "</a>"));
+                    $("#quran_ref_quotes").fadeIn(1000);
+                });
+
+                $("#quran_quotes").hide();
+                $("#quran_quotes").html(randomLine.replace(/#/g, "<br>"));
+                $("#quran_quotes").fadeIn(1000);
+
+            });
+
+        });
+    });
+});
+
+
+
+
