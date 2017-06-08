1830c022c9a495a7b107bf6faa74531205703ac4
Shaz <shahebaz13@gmail.com>
2017-06-08 19:56:55 +0530

    Version 2.0

--- quote.js
+++ quote.js
@@ -9,13 +9,14 @@ $(document).ready(function () {
                     randomIndex = Math.floor(Math.random() * lines.length),
                     randomLine = lines[randomIndex];
                 console.log(randomIndex, randomLine);
+                $("#quran_ref_quotes").hide();
                 $.get("quran_ref_quotes.txt", function (wholeTextFile) {
                     var lines = wholeTextFile.split(/\n/),
                     random_ref = lines[randomIndex];
                     console.log(random_ref, randomLine);
                     var url = "https://quran.com/" + random_ref
                     var target = "_blank"
-                    $("#quran_ref_quotes").hide();
+            
                     $("#quran_ref_quotes").html(" <a href ="+ url + " target=" + target + " > Quran " + random_ref.replace(/#/g, "<br>" + "</a>"));
                     $("#quran_ref_quotes").fadeIn(1000);
                 });
